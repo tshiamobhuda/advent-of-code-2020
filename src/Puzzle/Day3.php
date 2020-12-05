f50107fee9b7823e815a6769e223b57d60f19c1a
tshiamobhuda <tshiamobhuda@gmail.com>
2020-12-05 20:52:47 +0200

    Added Solution for day 3 part 2 - move logic to count trees into own function - use slope details to determine to row & pos to check for a tree - added array of slopes - loop through slopes & calculate the product of trees found in each slope

--- src/Puzzle/Day3.php
+++ src/Puzzle/Day3.php
@@ -6,24 +6,17 @@ namespace Aoc\Puzzle;
 
 class Day3 implements PuzzleInterface
 {
+    private array $slopes = [
+        ['right' => 1, 'down' => 1],
+        ['right' => 3, 'down' => 1],
+        ['right' => 5, 'down' => 1],
+        ['right' => 7, 'down' => 1],
+        ['right' => 1, 'down' => 2],
+    ];
+
     public function partA(array $data): ?string
     {
-        $count = 0;
-        $pos = 3;
-
-        for ($row = 1; $row < count($data); $row++) {
-            $rowSize = count(str_split($data[$row]));
-
-            if ($pos >= $rowSize) {
-                $pos = $pos - $rowSize;
-            }
-
-            if ($data[$row][$pos] && '#' === $data[$row][$pos]) {
-                $count++;
-            }
-
-            $pos += 3;
-        }
+        $count = $this->countTrees($data, $this->slopes[1]);
 
         if ($count > 0) {
             return $count . '';
@@ -34,6 +27,42 @@ class Day3 implements PuzzleInterface
 
     public function partB(array $data): ?string
     {
+        $product = 0;
+
+        foreach ($this->slopes as $slope) {
+            $count = $count = $this->countTrees($data, $slope);
+
+            if ($count > 0) {
+                $product = ($product === 0) ? $count : $product * $count ;
+            }
+        }
+
+        if ($product > 0) {
+            return $product . '';
+        }
+
         return null;
+    }
+
+    private function countTrees(array $data, array $slope): int
+    {
+        $count = 0;
+        $pos = $slope['right'];
+
+        for ($row = $slope['down']; $row < count($data); $row += $slope['down']) {
+            $rowSize = count(str_split($data[$row]));
+
+            if ($pos >= $rowSize) {
+                $pos = $pos - $rowSize;
+            }
+
+            if ($data[$row][$pos] && '#' === $data[$row][$pos]) {
+                $count++;
+            }
+
+            $pos += $slope['right'];
+        }
+
+        return $count;
     }
 }
