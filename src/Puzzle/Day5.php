fe2573e6d7f099c0ea26bfaac8889e0914588671
tshiamobhuda <tshiamobhuda@gmail.com>
2020-12-07 00:50:08 +0200

    Added Solution for Day 5 part 2 - separate logic to generate seadId into own method - generate all seatIds from boarder passports - sort generated seatIds - create & compare a virtual seats list with generated seatIds. Use min & max generated seatIds to create range of virtual seats - find the missing seat by comparing which seat is not present in generated seatsIds but present in virtual seats

--- src/Puzzle/Day5.php
+++ src/Puzzle/Day5.php
@@ -8,37 +8,7 @@ class Day5 implements PuzzleInterface
 {
     public function partA(array $data): ?string
     {
-        $seatIds = [];
-
-        foreach ($data as $boardingPass) {
-            $passData = str_split($boardingPass);
-
-            $rows = [0, 128];
-            for ($x = 0; $x < 7; $x++) {
-                if ('F' === $passData[$x]) {
-                    $rows[1] = ($rows[0] + $rows[1]) / 2;
-                }
-
-                if ('B' === $passData[$x]) {
-                    $rows[0] = ($rows[0] + $rows[1]) / 2;
-                }
-            }
-
-            $cols = [0, 8];
-            for ($x = count($passData) - 3; $x < count($passData); $x++) {
-                if ('L' === $passData[$x]) {
-                    $cols[1] = ($cols[0] + $cols[1]) / 2;
-                }
-
-                if ('R' === $passData[$x]) {
-                    $cols[0] = ($cols[0] + $cols[1]) / 2;
-                }
-            }
-
-            if ($rows[0] === ($rows[1] -1) && $cols[0] === ($cols[1] -1)) {
-                $seatIds[] = ($rows[0] * 8) + $cols[0];
-            }
-        }
+        $seatIds = $this->generateSeatIds($data);
 
         if (count($seatIds) > 0) {
             return max($seatIds) . '';
@@ -49,6 +19,52 @@ class Day5 implements PuzzleInterface
 
     public function partB(array $data): ?string
     {
+        $seatIds = $this->generateSeatIds($data);
+        sort($seatIds);
+
+        $tempSeatIds = range(min($seatIds), max($seatIds));
+        $missingSeatId = array_diff($tempSeatIds, $seatIds);
+
+        if (count($missingSeatId) === 1) {
+            return current($missingSeatId) . '';
+        }
+
         return null;
+    }
+
+    private function generateSeatIds(array $data): array
+    {
+        $seatIds = [];
+
+        foreach ($data as $boardingPass) {
+            $boardingPassData = str_split($boardingPass);
+            $rows = [0, 128];
+            for ($x = 0; $x < 7; $x++) {
+                if ('F' === $boardingPassData[$x]) {
+                    $rows[1] = ($rows[0] + $rows[1]) / 2;
+                }
+
+                if ('B' === $boardingPassData[$x]) {
+                    $rows[0] = ($rows[0] + $rows[1]) / 2;
+                }
+            }
+
+            $cols = [0, 8];
+            for ($x = count($boardingPassData) - 3; $x < count($boardingPassData); $x++) {
+                if ('L' === $boardingPassData[$x]) {
+                    $cols[1] = ($cols[0] + $cols[1]) / 2;
+                }
+
+                if ('R' === $boardingPassData[$x]) {
+                    $cols[0] = ($cols[0] + $cols[1]) / 2;
+                }
+            }
+
+            if ($rows[0] === ($rows[1] - 1) && $cols[0] === ($cols[1] - 1)) {
+                $seatIds[] = ($rows[0] * 8) + $cols[0];
+            }
+        }
+
+        return $seatIds;
     }
 }
